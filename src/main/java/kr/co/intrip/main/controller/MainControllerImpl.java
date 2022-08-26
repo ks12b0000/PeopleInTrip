a97a77eb8d862541935e865564778d00c0806b6c
박현준 <parkhyunjoon@baghyeonjun-ui-MacBookAir.local>
2022-08-26 14:47:41 +0900

    수정

--- src/main/java/kr/co/intrip/main/controller/MainControllerImpl.java
+++ src/main/java/kr/co/intrip/main/controller/MainControllerImpl.java
@@ -37,6 +37,7 @@ public class MainControllerImpl implements MainController {
 	public ModelAndView listMain(HttpServletRequest request,Model model,ApiDTO apiDTO, BusanApiDTO busanApiDTO, HttpServletResponse response) throws Exception {
 		
 		String viewName = (String) request.getAttribute("viewName");
+		
 		List<MainDTO> mainsList = mainService.listMain();
 		List<MainDTO> mainsList1 = mainService.listMain1();
 		List<ApiDTO> mainlist = tourservice.jejutourist_main(apiDTO);
@@ -45,15 +46,18 @@ public class MainControllerImpl implements MainController {
 		List<BusanApiDTO> festivalmain2 = tourservice.busanfestivalmain(busanApiDTO);
 		List<ApiDTO> exhibitionmain = tourservice.jejuexhibitionmain(apiDTO);
 		List<BusanApiDTO> experiencemain = tourservice.busanexperiencemain(busanApiDTO);
+		
 		model.addAttribute("mainlist", mainlist);
 		model.addAttribute("mainlist5", mainlist5);
 		model.addAttribute("festivalmain", festivalmain);
 		model.addAttribute("festivalmain2",festivalmain2);
 		model.addAttribute("exhibitionmain",exhibitionmain);
 		model.addAttribute("experiencemain",experiencemain);
+		
 		ModelAndView mav = new ModelAndView(viewName);
 		mav.addObject("mainsList", mainsList);
 		mav.addObject("mainsList1", mainsList1);
+		
 		return mav;		
 	}
 }
